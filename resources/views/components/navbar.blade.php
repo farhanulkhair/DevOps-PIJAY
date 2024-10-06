4935f7d941a5c70e58af243ccb67b4996b2c9b55
Alfi Zamriza <alfi576@mhs.usk.ac.id>
2024-10-06 13:09:46 +0700

    feat: add atribut

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,31 +11,31 @@
 </head>
 <body>
     <!-- Navbar -->
-    <div class="flex items-center px-4 py-0 md:px-40 bg-customNav fixed top-0 left-0 w-full z-40">
+    <div class="flex items-center px-4 py-0 md:px-40 bg-Biru fixed top-0 left-0 w-full z-40 rounded-b-full shadow-2xl ">
         {{-- Logo --}}
         <div class="flex-shrink-0 px-3 py-3">
             <a href="javascript:scroll(1)">
                 <img src="{{ asset('images/logoHotel.jpeg') }}" alt="logo" class="w-14 h-14 rounded-full">
-            </a>    
+            </a>
         </div>
         {{-- End of logo --}}
-        
+
         <!-- Menu -->
         <div class="flex-1 flex justify-end text-center">
             <ul class="flex justify-center gap-5 font-semibold text-white text-lg">
-                <li class="p-2 m-4 hover:scale-90 duration-[0.2s] rounded-md">
+                <li class="p-2 m-4 hover:scale-90 hover:text-hijau duration-[0.2s] rounded-md">
                     <a href="javascript:scroll(1)">Home</a>
                 </li>
-                <li class="p-2 m-4 hover:scale-90 duration-[0.2s] rounded-md">
+                <li class="p-2 m-4 hover:scale-90 hover:text-hijau duration-[0.2s] rounded-md">
                     <a href="javascript:scroll(2)">Booking</a>
                 </li>
-                <li class="p-2 m-4 hover:scale-90 duration-[0.2s] rounded-md">
+                <li class="p-2 m-4 hover:scale-90 hover:text-hijau duration-[0.2s] rounded-md">
                     <a href="javascript:scroll(3)">History</a>
                 </li>
             </ul>
         </div>
         <!-- End of menu -->
-    </div>    
+    </div>
     <!-- End of navbar -->
 
     <script>
